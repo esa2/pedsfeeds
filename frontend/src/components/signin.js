4a0aacfd8abbec064cc5226f2280c220883fe8b4
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-04-20 12:48:15 -0700

    add signin

---
+++ frontend/src/components/signin.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { Auth } from 'aws-amplify'
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import '../styles/signin.css'
+
+export default class Signin extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  validateForm() {
+    return this.state.email.length > 8 && this.state.password.length > 7
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      await Auth.signIn(this.state.email, this.state.password)
+      alert('Sign in completed')
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  render() {
+    return (
+      <div className="signin">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Sign in
+          </Button>
+        </form>
+      </div>
+    )
+  }
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/home'
+import Signin from './components/signin'
 import RouteError from './components/route-error'
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home}/>
+    <Route path="/signin" exact component={Signin}/>
     <Route component={RouteError}/>
   </Switch>

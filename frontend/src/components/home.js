c9fb973bf608dab206d91a573b448958da5cace5
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-04-20 11:04:37 -0700

    home page

---
+++ frontend/src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import "../styles/home.css"
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="lander">
+          <h1>Pedsfeeds</h1>
+          <p>Resources, support, experts for pediatric feeding and swallowing</p>
+        </div>
+      </div>
+    )
+  }
+}

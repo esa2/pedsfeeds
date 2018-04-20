e4da2794972710e02f2050c58aa0db411ca93033
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-04-20 11:08:21 -0700

    home page

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import "../styles/home.css"
+import React, { Component } from 'react'
+import '../styles/home.css'
 
 export default class Home extends Component {
   render() {

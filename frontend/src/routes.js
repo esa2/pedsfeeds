0e925ccfbbf729bea951a79827efde04eef33d98
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-04-21 07:09:22 -0700

    add logout

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/home'
 import Signin from './components/signin'
+import AppliedRoute from './components/applied-route'
 import RouteError from './components/route-error'
 
-export default () =>
+export default ({ childProps }) =>
   <Switch>
-    <Route path="/" exact component={Home}/>
-    <Route path="/signin" exact component={Signin}/>
+    <AppliedRoute path="/" exact component={Home} props={childProps}/>
+    <AppliedRoute path="/signin" exact component={Signin} props={childProps}/>
     <Route component={RouteError}/>
   </Switch>

7ee1a30d6c949d3673fc6ca0262045c8aad752c7
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-04-24 12:41:36 -0700

    add lander

---
+++ frontend/src/containers/lander.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+
+  render() {
+    return (
+      <div>
+      <h3>Resources, support, experts for pediatric feeding and swallowing</h3>
+
+      <p>At the Pediatric Feeding Association, our mission is to provide help for babies, children, and adolescents who struggle with feeding and swallowing, whether they eat orally or are tube fed via NG tubes, g-tubes, and gj-tubes.</p>
+
+      <p>We aim to create a connected community of families and pediatric feeding professionals, comprising doctors, feeding therapists, nutritionists, speech therapists, psychologists, and other specialists who evaluate and treat dysphagia and feeding disorders, including reflux, oral aversion, and aspiration.</p>
+
+      <p>Here you'll find a directory of feeding providers, along with other resources and information to support your journey to safe and happy feeding experiences for kids who can't or won't eat and drink enough to thrive on their own.</p>
+      </div>
+    )
+  }
+}

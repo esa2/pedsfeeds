d8b48ca625491e5d4c82711ba70ea0dd709c3330
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-11-30 13:06:46 -0800

    add job

--- frontend/src/containers/jobs.js
+++ frontend/src/containers/jobs.js
@@ -8,9 +8,15 @@ const Jobs = () => {
         greater Seattle Community. They are not sponsored or endorsed by the
         Pediatric Feeding Association.
       </h4>
-      <p>Feeding Therapist - Pediatric - OT/PT/SLP</p>
-      <br/>
-      <p>https://app.jobvite.com/j?cj=or57afwx&s=Pediatric_Feeding_Association</p>
+      <p>
+        <a
+          href="https://app.jobvite.com/j?cj=or57afwx&s=Pediatric_Feeding_Association"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Feeding Therapist - Pediatric - OT/PT/SLP
+        </a>
+      </p>
     </div>
   )
 }
